4309cd5222334758bef5f31908eb354a835e0fcf
glebassty <glebassty@gmail.com>
2025-07-24 18:53:45 +0300

    update articles

--- src/lib/funcs.ts
+++ src/lib/funcs.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export async function getArticles() {
   try {
-    const res = await axios.get('http://localhost:1337/api/articles?populate=*');
-    return res.data.data; // Ошибка типизации поправить`
+    const res = await axios.get('http://localhost:1337/api/articles');
+    return res.data.data; // Ошибка типизации`
   } catch (err) {
     console.error('Ошибка при загрузке данных из Strapi:', err);
     return [];
