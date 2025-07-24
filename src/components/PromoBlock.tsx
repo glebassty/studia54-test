e08c03a1d82aae830e2bb5a8e766ef23b6954e6e
glebassty <glebassty@gmail.com>
2025-07-24 17:35:51 +0300

    fix validator error

--- src/components/PromoBlock.tsx
+++ src/components/PromoBlock.tsx
@@ -32,11 +32,10 @@ export default function PromoBlock({ description }: PromoBlockProps) {
         </div>
         <div className="ml-12 flex w-1/2 max-w-lg flex-col items-start justify-center">
           <p className="mt-10 mb-8 text-lg text-neutral-700">{description}</p>
-          <Link href="/projects">
-            <Button variant="outline" className="px-8 py-4 text-lg">
-              All projects
-            </Button>
-          </Link>
+
+          <Button asChild variant="outline" className="px-8 py-4 text-lg">
+            <Link href="/projects">All projects</Link>
+          </Button>
         </div>
       </div>
     </section>
