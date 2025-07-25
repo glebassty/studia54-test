bf77c71c26a8812db450d4c806edf52ee351e7d0
glebassty <glebassty@gmail.com>
2025-07-25 16:45:14 +0300

    mobile footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,20 @@ export default function Footer() {
   return (
     <footer className="border-t border-gray-300 bg-[#181818] font-sans font-normal">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between">
+        {/* Мобильная версия */}
+        <div className="flex h-24 items-end justify-start md:hidden">
+          <div className="flex flex-col text-left">
+            <div className="text-2xl leading-none text-[#FFFFFF]">
+              architecture
+            </div>
+            <div className="mt-1 text-xs leading-none text-[#5B5C5D]">
+              © 2024, all rights reserved
+            </div>
+          </div>
+        </div>
+
+        {/* Десктопная версия */}
+        <div className="hidden h-16 items-center justify-between md:flex">
           <div className="flex items-center space-x-4">
             <div className="text-2xl leading-none text-[#FFFFFF]">
               architecture
